9c5fc4d419d1894e1b65c9aea35b4e480d7985e5
mivchal <mivchal@hotmail.com>
2020-09-30 20:03:56 +0200

    store refactor finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,19 @@
 import React from "react";
 import UserCreate from "./UserCreate";
-import LanguageContext from "../context/LanguageContext";
+import {LanguageStore} from "../context/LanguageContext";
 import ColorContext from "../context/ColorContext";
 import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {language: 'english'}
-
-        this.onLanguageChange = this.onLanguageChange.bind(this)
-    }
-
-    onLanguageChange(language) {
-        this.setState({
-            language
-        })
-
-
-    }
-
     render() {
         return (
             <div className="ui container">
-                <LanguageSelector onLanguageChange={this.onLanguageChange}/>
-
-                <LanguageContext.Provider value={this.state.language}>
+                <LanguageStore>
+                    <LanguageSelector/>
                     <ColorContext.Provider value={"red"}>
                         <UserCreate/>
                     </ColorContext.Provider>
-                </LanguageContext.Provider>
+                </LanguageStore>
             </div>
         );
     }
